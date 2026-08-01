68d19f94eb041a396beb12d94c34f21cfba48621
jnjnnj <jnjnnjzch@gmail.com>
2026-08-02 03:36:00 +0800

    Add KOReader plugin metadata

---
+++ koreader/tokenonkindle.koplugin/_meta.lua
@@ -0,0 +1,6 @@
+local _ = require("gettext")
+
+return {
+    fullname = _("Token on Kindle"),
+    description = _("Downloads the Token on Kindle dashboard and uses it as the KOReader sleep screen. Can also mirror the image to the Kindle linkss screensaver folder."),
+}
