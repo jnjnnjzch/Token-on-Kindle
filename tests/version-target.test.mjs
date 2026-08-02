9ed244a94b18968c733f873c5fe57f52a09868d1
jnjnnj <jnjnnjzch@gmail.com>
2026-08-02 23:48:54 +0800

    Target v0.6.2 reliability release

--- tests/version-target.test.mjs
+++ tests/version-target.test.mjs
@@ -4,6 +4,6 @@ import fs from 'node:fs';
 
 const pkg = JSON.parse(fs.readFileSync(new URL('../package.json', import.meta.url), 'utf8'));
 
-test('desktop integration release targets v0.6.1', () => {
-  assert.equal(pkg.version, '0.6.1');
+test('updater and tray reliability release targets v0.6.2', () => {
+  assert.equal(pkg.version, '0.6.2');
 });
