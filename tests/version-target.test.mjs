dc56d55caae1c96c662ce0ba6d92f3d35c1c2224
jnjnnj <jnjnnjzch@gmail.com>
2026-08-02 14:54:03 +0800

    Target v0.5.1 layout release

--- tests/version-target.test.mjs
+++ tests/version-target.test.mjs
@@ -4,6 +4,6 @@ import fs from 'node:fs';
 
 const pkg = JSON.parse(fs.readFileSync(new URL('../package.json', import.meta.url), 'utf8'));
 
-test('feature release targets v0.5.0', () => {
-  assert.equal(pkg.version, '0.5.0');
+test('layout release targets v0.5.1', () => {
+  assert.equal(pkg.version, '0.5.1');
 });
