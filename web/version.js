9cec24e086d80ba7cbba3a544ac414979af36fab
jnjnnj <jnjnnjzch@gmail.com>
2026-08-04 22:39:53 +0800

    Trigger v0.8.6 release from synchronized package metadata

--- web/version.js
+++ web/version.js
@@ -1,2 +1,2 @@
-// Generated from the release tag / Cargo package version. Do not edit manually.
+// Generated from package metadata before every build. Do not edit manually.
 export const APP_VERSION = "0.8.6";
